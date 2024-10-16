21c9de90ea3f6f3052bc90f95e47b7ac0704ab8a
KoDleGaCY7500 <notkodlegacy7500@gmail.com>
2024-10-16 05:26:47 -0700

    intro to bit-manipulation

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-num1=10
-num2=5
-print(num1&num2,"& operator")
+# num1=10
+# num2=5
+# print(num1&num2,"& operator")
 
-print(num1|num2,"| or opeerator")
+# print(num1|num2,"| or opeerator")
 
-print(~num1)
+# print(~num1)
 
-print(num1 <<3)
-print(num2>>2)
+# print(num1 <<3)
+# print(num2>>2)
+
+def evenodd(n):
+    if(n^1==n+1):
+        return True
+    else:
+        return False
+    
+number=int(input("Enter a number: "))
+if evenodd(number):
+    print("is even")
+else:
+    print("is odd")
